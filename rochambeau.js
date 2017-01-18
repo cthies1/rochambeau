9a2182b49a72780b9bf70b3f15bbc9969a77df65
Chloe Thies <ChloeThies@Chloes-MacBook.local>
2017-01-18 15:14:02 -0800

    beautified

--- rochambeau.js
+++ rochambeau.js
@@ -1,57 +1,62 @@
 // This is the varaible that stores the score.
 // score[0] = wins, score[1] = ties, score[2] = losses
-var score = [0,0,0];
-
+var score = [0, 0, 0];
 // The variables store the current player's and computer's choices
 // 0 = Rock, 1 = Paper, 2 = Scissors
 var playerChoice;
 var computerChoice;
 
-function playGame(){
+function playGame() {
     // Here is the game ruleset algorithm
     if (playerChoice == computerChoice) {
         // We have a tie!
         console.log("tie");
         return 0;
-    } else if (playerChoice == 0 && computerChoice == 2 || computerChoice == 4) {
+    }
+    else if (playerChoice == 0 && computerChoice == 2 || computerChoice == 4) {
         // Rock beats Scissors/Lizard - a win!
         console.log("win");
         return 1;
-    } else if (playerChoice == 1 && computerChoice == 0 || computerChoice == 3) {
+    }
+    else if (playerChoice == 1 && computerChoice == 0 || computerChoice == 3) {
         // Paper beats Rock/Spock - a win!
         console.log("win");
         return 1;
-    } else if (playerChoice == 2 && computerChoice == 1 || computerChoice == 4) {
+    }
+    else if (playerChoice == 2 && computerChoice == 1 || computerChoice == 4) {
         // Scissors beats Paper/Lizard - a win!
         console.log("win");
         return 1;
-    } else if (playerChoice == 3 && computerChoice == 2 || computerChoice == 0) {
+    }
+    else if (playerChoice == 3 && computerChoice == 2 || computerChoice == 0) {
         // Spock beats Scissors/Rock - a win!
         console.log("win");
         return 1;
-    } else if (playerChoice == 4 && computerChoice == 1 || computerChoice == 3) {
+    }
+    else if (playerChoice == 4 && computerChoice == 1 || computerChoice == 3) {
         // Lizard beats Paper/Spock - a win!
         console.log("win");
         return 1;
-    } else {
+    }
+    else {
         // All other combinations are losses
         console.log("lose");
         return -1;
     }
 }
 
-function displayScoreBoard(winsId, lossesId, tiesId){
+function displayScoreBoard(winsId, lossesId, tiesId) {
     document.getElementById(winsId).innerHTML = score[0];
     document.getElementById(lossesId).innerHTML = score[2];
     document.getElementById(tiesId).innerHTML = score[1];
 }
 
-function updateScore(val){
+function updateScore(val) {
     ++score[val];
     console.log("The score is now " + score);
 }
 
-function displayGameResult(resultId){
+function displayGameResult(resultId) {
     // Define an array of text labels for the choices 0, 1, 2;
     var choices = ["Rock", "Paper", "Scissors"];
     // Now play the game and store the result
@@ -64,12 +69,14 @@ function displayGameResult(resultId){
         updateScore(0);
         document.getElementById(resultId).innerHTML = message + "YOU WIN!";
         document.getElementById(resultId).className = "alert alert-success";
-    } else if (result == -1) {
+    }
+    else if (result == -1) {
         updateScore(2);
         // Display that it was a loss
         document.getElementById(resultId).innerHTML = message + "YOU LOOSE! ";
         document.getElementById(resultId).className = "alert alert-danger";
-    } else {
+    }
+    else {
         // Display that it was a tie
         updateScore(1);
         document.getElementById(resultId).innerHTML = message + "A tie. ";
@@ -85,6 +92,6 @@ function storePlayerChoice(choice) {
 
 function storeComputerChoice() {
     // Generate computer's random choice
-    computerChoice = Math.floor(Math.random()*3);
+    computerChoice = Math.floor(Math.random() * 3);
     console.log("Computer choice = " + computerChoice);
 }
